b0ba27a7007c844348eccd4b1cf4d347c422aafc
ryan <ryan.boudiaf@opretdevous.com>
2023-02-17 18:32:44 +0100

    edit winchapps.sh

--- shell/run/winchapps_bsi.sh
+++ shell/run/winchapps_bsi.sh
@@ -1,27 +1,20 @@
 #!/bin/bash
-title "Running winchapps_bsi"
+title "Running $project_name"
 
 cd projects/$project_name || exit
 
-isRunning=$(docker ps -a -aqf name="winchapps_bsi")
+isRunning=$(docker ps -a -aqf name="$project_name")
 
 if [ ! -z $isRunning ]; then
-  docker kill winchapps_bsi && docker rm winchapps_bsi
+  docker kill "$project_name" && docker rm "$project_name"
 fi
 
-#changing env variable
-#perl -pi -e 's/`opdv/`dev.opdv/g' ./_build/docker/apache/Dockerfile
-# changing sendmail config
-#perl -pi -e 's/sendmail -i -t/sendmail -i -t --smtp-addr=mailhog:1025/g' ./_build/docker/apache/app_php.ini
+docker build -t "$project_name" --no-cache -f ./_build/docker/apache/Dockerfile .
 
-docker build -t winchapps_bsi --no-cache -f ./_build/docker/apache/Dockerfile .
-
-#Install pdftk
 apt-get update
-apt-get install pdftk
 
 #checkout files
 #git checkout ./_build/docker/apache/Dockerfile
 #git checkout ./_build/docker/apache/app_php.ini
 
-docker run -d --name winchapps_bsi -p 8081:80 --network land_default -v "/$(pwd)/:/var/www/basededonneesunique" --restart always winchapps_bsi
+docker run -d --name "$project_name" -p 8081:80 --network land_winchapps_default -v "/$(pwd)/:/var/www/basededonneesunique" --restart always "$project_name"
